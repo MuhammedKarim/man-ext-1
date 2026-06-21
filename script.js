1d29928a847ca112122fae9252bc6c82bc49d97d
Muhammed Karim <muhammedkarim@hotmail.co.uk>
2026-06-21 15:32:19 +0100

    remove majlis poster

--- script.js
+++ script.js
@@ -454,7 +454,7 @@ function initPrayerTimes() {
   function startKkBayanRotation() {
     if (kkBayanRotateInterval) return;
 
-    const files = ['kk-bayan2'];
+    const files = ['kk-bayan'];
 
     showKalimatImage(files[kkBayanIndex]);
 
